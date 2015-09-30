85381273804dd95ba606edbbd8d427405c19a4b1
yusuke furukawa <littlefive.jp@gmail.com>
2015-09-30 22:49:33 +0900

    fix

--- gyazo.py
+++ gyazo.py
@@ -1,30 +1,36 @@
-#coding:utf-8
+# coding:utf-8
 import cgi
 import boto
 import hashlib
 import datetime
 from wsgiref.simple_server import make_server
 import boto.s3.connection
-from boto.s3.connection import S3Connection
 
 ACCESS_KEY = ""
 SECRET_KEY = ""
 BUCKET_NAME = ""
 RETURN_BASE_URL = ""
 
-# if need
-#S3Connection.DefaultHost='s3-ap-northeast-1.amazonaws.com'
+# Possibly it may be necessary.
+# from boto.s3.connection import S3Connection
+# S3Connection.DefaultHost='s3-ap-northeast-1.amazonaws.com'
 
 
 def current_time():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
+def key_name(data, gyazo_id):
+    return "".join([hashlib.sha224(data +
+                                   gyazo_id +
+                                   current_time()).hexdigest(), ".png"])
+
+
 def gyazo(environ, start_response):
+
     status = "200 OK"
     headers = [("Content-type", "image/png")]
     start_response(status, headers)
-    
     if environ["REQUEST_METHOD"] == "POST":
         post_env = environ.copy()
         post_env["QUERY_STRING"] = ""
@@ -33,25 +39,29 @@ def gyazo(environ, start_response):
                                 keep_blank_values=True
                                 )
         if "imagedata" in post.keys():
-            imagedata = post["imagedata"].value
-            gyazo_id = post["id"].value
-            conn = boto.connect_s3(aws_access_key_id=ACCESS_KEY,
-                                   aws_secret_access_key=SECRET_KEY,
-                                   calling_format=boto.s3.connection.OrdinaryCallingFormat(),
-                                   )
+            imagedata = post.get("imagedata", False)
+            if not imagedata:
+                return [RETURN_BASE_URL]
+            gyazo_id = post.get("id", "")
+            conn = boto.connect_s3(
+                aws_access_key_id=ACCESS_KEY,
+                aws_secret_access_key=SECRET_KEY,
+                calling_format=boto.s3.connection.OrdinaryCallingFormat())
             gyazo = conn.get_bucket(BUCKET_NAME)
-            h = hashlib.sha224(imagedata + gyazo_id + current_time()).hexdigest()
-            while gyazo.get_key(h):
-                h = hashlib.sha224(imagedata + gyazo_id + current_time()).hexdigest()
-            k = gyazo.new_key("".join([h, ".png"]))
+            key_id = key_name(imagedata, gyazo_id)
+            while gyazo.get_key(key_id):  # Does key not repeat?
+                key_id = key_name(imagedata, gyazo_id)
+            k = gyazo.new_key(key_id)
             k.set_metadata("Content-Type", "image/png")
             k.set_contents_from_string(imagedata)
             k.make_public()
             k.close()
             return ["".join([RETURN_BASE_URL, k.name])]
+    status = "200 OK"
     return [RETURN_BASE_URL]
 
-httpd = make_server("", 8080, gyazo)
-print "Serving on port 8080..."
 
-httpd.serve_forever()
+if __name__ == "__main__":
+    httpd = make_server("", 8080, gyazo)
+    print "Serving on port 8080..."
+    httpd.serve_forever()
